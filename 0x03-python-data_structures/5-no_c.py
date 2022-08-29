72de3e9ad92a3ece6591f53e48f8481e52bfcb3e
Siraaj <sirjeftha@gmail.com>
2022-08-29 07:25:50 -0700

    retry commit

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-
 def no_c(my_string):
-    copy_string = [x for x in my_string if x != 'c' or x != 'C']
-    return("".join(copy_string))
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
+    return new_string
