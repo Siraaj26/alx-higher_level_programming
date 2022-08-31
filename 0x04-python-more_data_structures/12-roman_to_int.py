12d0e7d3abd51a1ac2894fa2c746a1ccff898c45
Siraaj <sirjeftha@gmail.com>
2022-08-31 08:02:38 -0700

    adjusted lines

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,8 @@
 
 
 def roman_to_int(roman_string):
-    if (not isinstance(roman_string, str) or roman_string is None):
+    if (not isinstance(roman_string, str) or
+        roman_string is None):
         return (0)
 
     roman_dict = {
