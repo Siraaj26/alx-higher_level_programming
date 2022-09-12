7b3c26d3a241d74a89e8fee821c1c53fe3d95703
Siraaj <sirjeftha@gmail.com>
2022-09-12 09:00:57 -0700

    added return statement last line

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,7 +3,7 @@ def safe_function(fct, *args):
     import sys
     try:
         result = fct(*args)
-        return (result)
     except:
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return (None)
+    return (result)
