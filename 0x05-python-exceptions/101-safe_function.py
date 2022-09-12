0d2855d068fd10d59b8e094e54b867c147e5941d
Siraaj <sirjeftha@gmail.com>
2022-09-12 11:32:28 -0700

    changed script

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,7 +3,7 @@ def safe_function(fct, *args):
     import sys
     try:
         result = fct(*args)
-    except:
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (None)
+    except Exception as i:
+         sys.stderr.write("Exception: {}\n".format(i))
+         result = None
     return (result)
