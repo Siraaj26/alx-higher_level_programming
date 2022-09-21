da12254fcb5268a587abaf5b1fba8be7e75ea059
Siraaj <sirjeftha@gmail.com>
2022-09-21 01:26:48 -0700

    revert

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 """
     Defines a function that calculates addition
-    of two integers.
+    of two integers
 """
 
 
 def add_integer(a, b=98):
-    """Returns the sum of two integers"""
+    """Calculates sum of two integers"""
 
-    if isintstance(a, float):
-        a = int(a)
-    if isinstance(b, float):
-        b = int(b)
-    if not (isinstance(a, int)):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if not (isinstance(b, int):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
+
+    a = int(a) if isinstance(a, float) else a
+    b = int(b) if isinstance(b, float) else b
+
     return (a + b)
