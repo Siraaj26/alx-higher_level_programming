5cd541c107a8954c892477bc31bc508a4949ab8a
Siraaj <sirjeftha@gmail.com>
2022-10-03 04:54:15 -0700

    Update the class Base by adding the static method def from_json_string(json_string)

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,26 +22,34 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
-     @staticmethod
-     def to_json_string(list_dictionaries):
-         """
-         returns the JSON string representation of list_dictionaries
-         """
-         if list_dictionaries is None or len(list_dictionaries) == 0:
-             return "[]"
-         return json.dumps(list_dictionaries)
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        returns the JSON string representation of list_dictionaries
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
 
-       @classmethod
-       def save_to_file(cls, list_objs):
-           """
-           writes the JSON string representation of list_objs to a file
-           """
-           filename = cls.__name__ + ".json"
-           if list_objs is None or len(list_objs) == 0:
-               with open(filename, "w") as f:
-                   f.write("[]")
-           else:
-               with open(filename, "w") as f:
-                   f.write(cls.to_json_string(list(map(lambda x:
-                                                       x.to_dictionary(),
-                                                       list_objs))))
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        writes the JSON string representation of list_objs to a file
+        """
+        filename = cls.__name__ + ".json"
+        if list_objs is None or len(list_objs) == 0:
+            with open(filename, "w") as f:
+                f.write("[]")
+        else:
+            with open(filename, "w") as f:
+                f.write(cls.to_json_string(list(map(lambda x:
+                                                    x.to_dictionary(),
+                                                    list_objs))))
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        returns the list of the JSON string representation json_string
+        """
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
