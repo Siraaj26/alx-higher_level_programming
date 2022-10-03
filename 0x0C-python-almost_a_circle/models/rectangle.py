1c64c99872d21a365f8229dc96e75428443f245f
Siraaj <sirjeftha@gmail.com>
2022-10-03 02:57:33 -0700

    update class rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -101,3 +101,68 @@ class Rectangle(Base):
                 raise ValueError("y must be >= 0")
 
             self.__y = value
+
+        def area(self):
+            """
+            returns area of the rectangle
+            """
+            return self.__width * self.__height
+
+        def display(self):
+            """
+            prints rectangle to stdout with instance "#"
+            """
+            for i in range(self.__y):
+                print()
+            for i in range(self.__height):
+                print(" " * self.__x, end="")
+                print("#" * self.__width)
+
+        def __str__(self):
+            """
+            returns string representation of the rectangle
+            """
+            return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x,
+                                                           self.__y, self.__width,
+                                                           self.__height)
+
+        def update(self, *args, **kwargs):
+            """
+            assigns an argument to each attribute
+            """
+            if args:
+                for i, arg in enumerate(args):
+                    if i == 0:
+                        self.id = arg
+                    if i == 1:
+                        self.__width = arg
+                    if i == 2:
+                        self.__height = arg
+                    if i == 3:
+                        self.__x = arg
+                    if i == 4:
+                        self.__y = arg
+
+            else:
+                for key, value in kwargs.items():
+                    if key == "id":
+                        self.id = value
+                    if key == "width":
+                        self.__width = value
+                    if key == "height":
+                        self.__height = value
+                    if key == "x":
+                        self.__x = value
+                    if key == "y":
+                        self.__y = value
+
+        def to_dictionary(self):
+            """
+            returns the dictionary representation of the rectangle
+            """
+            return {"x": self.__x,
+                    "y": self.__y,
+                    "id": self.id,
+                    "height": self.__height,
+                    "width": self.__width
+                   }
