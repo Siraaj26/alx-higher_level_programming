64a2acd6db18a270e1c7d400ceeeb40cbdcd84df
Siraaj <sirjeftha@gmail.com>
2022-10-03 04:39:39 -0700

    update base adding static method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,6 +3,11 @@
 Base class for all models
 """
 
+import json
+import csv
+import os
+import turtle
+
 class Base:
     """
     Base class for all models
@@ -16,3 +21,27 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+     @staticmethod
+     def to_json_string(list_dictionaries):
+         """
+         returns the JSON string representation of list_dictionaries
+         """
+         if list_dictionaries is None or len(list_dictionaries) == 0:
+             return "[]"
+         return json.dumps(list_dictionaries)
+
+       @classmethod
+       def save_to_file(cls, list_objs):
+           """
+           writes the JSON string representation of list_objs to a file
+           """
+           filename = cls.__name__ + ".json"
+           if list_objs is None or len(list_objs) == 0:
+               with open(filename, "w") as f:
+                   f.write("[]")
+           else:
+               with open(filename, "w") as f:
+                   f.write(cls.to_json_string(list(map(lambda x:
+                                                       x.to_dictionary(),
+                                                       list_objs))))
