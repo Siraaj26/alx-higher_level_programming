5325711e6f3b00196a20bafb9dd2ce5d4a07c5e0
Siraaj <sirjeftha@gmail.com>
2022-12-15 05:59:19 -0800

    updated script

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a req to URL, display response status code only
-curl -s -o /dev/null -w "%{http_code}" "$1
+# Sends a GET request to a given URL and display the response status code.
+curl -s -o /dev/null -w "%{http_code}" "$1"
